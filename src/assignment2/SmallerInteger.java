17b28e91e893f95f42f1e4b2c58b89af9baf28b7
Tolueni <tolueni@hotmail.com>
2015-10-20 12:56:51 -0400

    Update SmallerInteger.java

--- src/assignment2/SmallerInteger.java
+++ src/assignment2/SmallerInteger.java
@@ -4,14 +4,14 @@ package assignment2;
  */
 
 
-import java.util.Scanner;
+import java.util.Scanner; //importing the java scanner enables the program to request user input as well as produce an output. In general, the scanner serves as an interactive base between the program and the user.
 
 public class SmallerInteger 
 {
-	public static void main(String args[])
+	public static void main(String args[]) //main method of the program. 
 	{
 		
-		Scanner number = new Scanner(System.in);
+		Scanner number = new Scanner(System.in); //The declaration of the new scanner enmables the user to type an input into the program.
 		
 		System.out.println("Enter the first integer");
 		int num1 = number.nextInt();
@@ -19,7 +19,7 @@ public class SmallerInteger
 		System.out.println("Enter the second number");
 		int num2 = number.nextInt();
 		
-		if (num1 > num2)
+		if (num1 > num2)//the following if-else statements create the conditions for measuring the conditions inputted by the user. The condition num1 > num2  requests the program to run a boolean and then follwot the unstructions of the if command.
 		{
 			System.out.println("The smallest number you inputed is " +num2);
 		}
