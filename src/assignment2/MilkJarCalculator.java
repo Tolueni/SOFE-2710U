a2c07687270a1032ed7e140b93f02498632b9fb7
Tolueni <tolueni@hotmail.com>
2015-10-20 12:51:26 -0400

    Update MilkJarCalculator.java

--- src/assignment2/MilkJarCalculator.java
+++ src/assignment2/MilkJarCalculator.java
@@ -2,14 +2,14 @@ package assignment2;
 
 public class MilkJarCalculator {
 
-	public static void main(String[] args)
+	public static void main(String[] args)//main method of the program
 	{
-		double milk =5.5; // gallons
-		double jarCapacity = 0.75; //gallons
+		double milk =5.5; //defines the variable milk as a double variabe, unit in gallons, and initializes it at 5.5 gallons
+		double jarCapacity = 0.75; //define the variable jarCapacity as a double variable, unit in gallons, and initializes it at 0.75 gallons
 		
-		double completelyFilledJars = milk /jarCapacity;
+		double completelyFilledJars = milk /jarCapacity; //defines formula for finding a completely filled jarsby using mathematical operator '/' and assignment operator '='.
 		
-		System.out.println(completelyFilledJars);
+		System.out.println(completelyFilledJars); //output of the program when is run is set to calculate and then show the value for the completely filled jar.
 		
 		
 	}
