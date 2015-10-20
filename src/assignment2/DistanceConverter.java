69a1bbf442ae830aad2cd900b76b437a1400aa59
Tolueni <tolueni@hotmail.com>
2015-10-20 12:47:21 -0400

    Update DistanceConverter.java

--- src/assignment2/DistanceConverter.java
+++ src/assignment2/DistanceConverter.java
@@ -4,21 +4,21 @@ package assignment2;
  * Oct 7, 2015
  */
 
-public class DistanceConverter 
+public class DistanceConverter //Declared DistanceConverter as the public class
 {
 
-	public static void main(String args[])
+	public static void main(String args[])// main method of the program
 	{
 	
-	final double yardtoinches = 3.0;
-	final double feettoinches = 12.0;	
+	final double yardtoinches = 3.0; //Defined yardtoinches as a double and 3.0 as the final value for this double variable.
+	final double feettoinches = 12.0; //Defines feettoinches as a double and 12.0 asthe final value for this double cariable
 	double yards=3.5;
 	
 	//convertions
-	double feet =yards * yardtoinches;
+	double feet =yards * yardtoinches; //defines the formulas for the conversion of feet to yard andinches to feet.
 	double inches = feet * feettoinches;
 	
-	System.out.println(yards +"yards are " + feet + " feet");
+	System.out.println(yards +"yards are " + feet + " feet"); // system.out is used to print the embedded information to the user. Makes use of the PrintStream import.
 	System.out.println(yards + "yards are " + inches + " inches");
 	
 	}
